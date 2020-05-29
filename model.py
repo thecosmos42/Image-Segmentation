7fb88312cfe87d0f8b28d97cf2d0c8c0acebdf2f
thecosmos42 <shithu20@rediffmail.com>
2020-05-29 23:53:44 +0400

    Update

---
+++ model.py
@@ -0,0 +1,107 @@
+from keras import backend as K
+import tensorflow as tf
+from tensorflow.keras.layers import BatchNormalization
+
+#IoU metric
+def iou_coef(y_true, y_pred, smooth=1):
+  intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2,3])
+  union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
+  iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+  
+  return iou
+
+#soft dice loss
+def dice_coef(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (
+                K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred, smooth=1.):
+    return 1- dice_coef(y_true, y_pred)
+
+#Build the U-NET model
+def model(height,width,channels):
+
+    inputs = tf.keras.layers.Input((height, width, channels))
+
+    s = tf.keras.layers.Lambda(lambda x: x/255)(inputs)
+
+    #Contraction path
+    c1 = tf.keras.layers.Conv2D(filters =16, kernel_size=(3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    c1 = BatchNormalization()(c1)
+    p1 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2))(c1)
+
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    c2 = BatchNormalization()(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+     
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    c3 = BatchNormalization()(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+     
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    c4 = BatchNormalization()(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    c5 = BatchNormalization()(c5)
+
+    #Expansive path 
+    #does transposed Convolution
+    u6 = tf.keras.layers.Conv2DTranspose(filters = 128, kernel_size = (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate(inputs = [u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    c6 = BatchNormalization()(c6)
+     
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    c7 = BatchNormalization()(c7)
+     
+    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    c8 = BatchNormalization()(c8)
+     
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    c9 = BatchNormalization()(c9)
+     
+    #since its the final layer and we are doing binary classification, acitvation function is sigmoid
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+     
+    '''
+    Till now we have initialized the layers/blocks of the U-Net. We have to wrap them into a Model.
+    One way to initialize a model is by using the input and output layer as parameters in the tf.keras.Model().
+    Now we can use functions like fit, compile, predict etc.
+    '''
+
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+
+
+    model.compile(optimizer='adam', loss=dice_loss, metrics=[iou_coef])
+
+    model.summary()
+
+    return model
